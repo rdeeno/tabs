1c52d5c7d4c743619a2bf2478745dddd9d2b3ac3
Denis <d.russu@webus.md>
2019-04-18 16:09:45 +0300

    tabs readme done

--- tabs.js
+++ tabs.js
@@ -1,21 +1,17 @@
-var tabBtn = [...document.querySelectorAll('button')];
+var tabBtns = [...document.querySelectorAll('button')];
 var tabContent = [...document.querySelectorAll('.tabcontent')];
 var current = 0;
-var clicked;
-tabBtn.forEach(elm => {
+tabBtns.forEach(elm => {
     elm.addEventListener('click', tabChange)
 })
 
-function tabChange(e) {
-    var target = e.target;
+function tabChange() {
+    let index = tabBtns.indexOf(this)
+    tabBtns[current].classList.remove('active')
+    tabContent[current].classList.remove('visible');
 
-    clicked = tabBtn.indexOf(target)
-    tabContent[current].classList.remove('visible')
-    tabBtn[current].classList.remove('active')
-    current++
-    if (current >= tabContent.length) {
-        current = clicked;
-    }
-    tabContent[current].classList.add('visible')
-    tabBtn[current].classList.add('active')
+    tabContent[index].classList.add('visible')
+    tabBtns[index].classList.add('active')
+
+    current = index;
 }
